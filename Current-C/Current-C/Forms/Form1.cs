a077d692ed90f3051f47ab53bdd9f010b601f922
Andy Tertzakian <andy.tertzakian@gmail.com>
2016-12-06 16:13:03 -0800

    Added Internet Connection Status check

---
+++ Current-C/Current-C/Forms/Form1.cs
@@ -0,0 +1,36 @@
+﻿/*
+ *
+ *Author: Andre Tertzakian
+ *  Date: December, 2016 
+ * 
+ */
+
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Current_C.Utility;
+
+namespace Current_C
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = (InternetUtility.InternetConnected()) ? "Connected" : "Not Connected";
+        }
+    }
+}
---
+++ Current-C/Current-C/Utility/InternetUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Current_C.Utility
+{
+    static class InternetUtility
+    {
+
+        private static int Error = 0;
+
+        public static bool InternetConnected()
+        {
+            long dwConnectionFlags = 0;
+            return !(!InternetGetConnectedState(dwConnectionFlags, 0) || InternetAttemptConnect(0) != Error);
+        }
+
+        [DllImport("wininet.dll", SetLastError = true)]
+        public static extern int InternetAttemptConnect(uint res);
+
+        [DllImport("wininet.dll", SetLastError = true)]
+        public static extern bool InternetGetConnectedState(long flags, long reserved);
+    }
+}
